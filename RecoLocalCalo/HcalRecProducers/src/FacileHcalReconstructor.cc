33d6a5b455a50eb8127bef245cd2e13394ff4c60
Jeffrey Krupa <jeffrey.krupa@cern.ch>
2020-10-13 21:57:56 -0500

    esConsumes

--- RecoLocalCalo/HcalRecProducers/src/FacileHcalReconstructor.cc
+++ RecoLocalCalo/HcalRecProducers/src/FacileHcalReconstructor.cc
@@ -12,99 +12,93 @@
 #include "Geometry/CaloTopology/interface/HcalTopology.h"
 #include "Geometry/Records/interface/HcalRecNumberingRecord.h"
 
-class FacileHcalReconstructor : public SonicEDProducer<TritonClient>
-{
+class FacileHcalReconstructor : public SonicEDProducer<TritonClient> {
 public:
-    explicit FacileHcalReconstructor(edm::ParameterSet const& cfg) : 
-        SonicEDProducer<TritonClient>(cfg),
+  explicit FacileHcalReconstructor(edm::ParameterSet const& cfg)
+      : SonicEDProducer<TritonClient>(cfg),
         fChannelInfoName_(cfg.getParameter<edm::InputTag>("ChannelInfoName")),
-        fTokChannelInfo_(consumes<HBHEChannelInfoCollection>(fChannelInfoName_))
-     
-    {
-        produces<HBHERecHitCollection>();
-        setDebugName("FacileHcalReconstructor");
+        fTokChannelInfo_(consumes<HBHEChannelInfoCollection>(fChannelInfoName_)),
+        htopoToken_(esConsumes<HcalTopology, HcalRecNumberingRecord>()) {
+    produces<HBHERecHitCollection>();
+    setDebugName("FacileHcalReconstructor");
+  }
+
+  void acquire(edm::Event const& iEvent, edm::EventSetup const& iSetup, Input& iInput) override {
+    edm::Handle<HBHEChannelInfoCollection> hChannelInfo;
+    iEvent.getByToken(fTokChannelInfo_, hChannelInfo);
+    const HBHEChannelInfoCollection* channelInfo = hChannelInfo.product();
+
+    const HcalTopology* htopo = &iSetup.getData(htopoToken_);
+
+    auto& input1 = iInput.begin()->second;
+    auto data1 = std::make_shared<TritonInput<float>>();
+    data1->reserve(channelInfo->size());
+    client_.setBatchSize(channelInfo->size());
+
+    hcalIds_.clear();
+
+    for (const auto& pChannel : *channelInfo) {
+      std::vector<float> input;
+      const HcalDetId pDetId = pChannel.id();
+      hcalIds_.push_back(pDetId);
+
+      //FACILE uses iphi as a continuous variable
+      input.push_back((float)pDetId.iphi());
+      input.push_back((float)pChannel.tsGain(0.));
+      for (unsigned int itTS = 0; itTS < pChannel.nSamples(); ++itTS) {
+        input.push_back((float)pChannel.tsRawCharge(itTS));
+      }
+
+      //FACILE considers 7 Hcal depths as binary variables
+      std::cout << htopo->maxDepth() << std::endl;
+      for (int itDepth = 1; itDepth < htopo->maxDepth(); itDepth++) {
+        input.push_back(pDetId.depth() == itDepth);
+      }
+
+      std::cout << htopo->lastHERing() << std::endl;
+      //ieta is also encoded as a binary variable
+      for (int itIeta = 0; itIeta <= htopo->lastHERing(); itIeta++) {
+        input.push_back(pDetId.ieta() == itIeta);
+      }
+
+      data1->push_back(input);
     }
 
-    void acquire(edm::Event const& iEvent, edm::EventSetup const& iSetup, Input& iInput) override {
+    input1.toServer(data1);
+  }
 
-	edm::Handle<HBHEChannelInfoCollection> hChannelInfo;
-	iEvent.getByToken(fTokChannelInfo_, hChannelInfo); 
-	const HBHEChannelInfoCollection *channelInfo = hChannelInfo.product();
+  void produce(edm::Event& iEvent, edm::EventSetup const& iSetup, Output const& iOutput) override {
+    std::unique_ptr<HBHERecHitCollection> out;
+    out = std::make_unique<HBHERecHitCollection>();
+    out->reserve(hcalIds_.size());
 
-        edm::ESHandle<HcalTopology> hcalTopology;
-        iSetup.get<HcalRecNumberingRecord>().get(hcalTopology);
-	const HcalTopology* theHcalTopology = hcalTopology.product();
+    const auto& output1 = iOutput.begin()->second;
+    const auto& outputs = output1.fromServer<float>();
 
-	auto& input1 = iInput.begin()->second;
-	auto data1 = std::make_shared<TritonInput<float>>();
-	data1->reserve(channelInfo->size());
-        client_.setBatchSize(channelInfo->size());
+    for (std::size_t iB = 0; iB < hcalIds_.size(); iB++) {
+      float rhE = outputs[iB][0];
+      if (rhE < 0. or std::isnan(rhE) or std::isinf(rhE))
+        rhE = 0;
 
-	hcalIds_.clear(); 
-
-	for(const auto& pChannel : *channelInfo){	
-
-	    std::vector<float> input;
-  	    const HcalDetId        pDetId = pChannel.id();
- 	    hcalIds_.push_back(pDetId);    
-
-	    //FACILE uses iphi as a continuous variable
-	    input.push_back((float)pDetId.iphi());
-            input.push_back((float)pChannel.tsGain(0.));
-	    for (unsigned int itTS=0; itTS < pChannel.nSamples(); ++itTS) {
-		input.push_back((float)pChannel.tsRawCharge(itTS));
-	    }
-  
-	    //FACILE considers 7 Hcal depths as binary variables
-            for (int itDepth=1; itDepth < theHcalTopology->maxDepth(); itDepth++){
-		if (pDetId.depth() == itDepth)  input.push_back(1.f);
-		else				input.push_back(0.f);
-	    }
-
-	    //ieta is also encoded as a binary variable
-	    for (int itIeta = 0; itIeta < theHcalTopology->lastHERing(); itIeta++){
-		if (std::abs(pDetId.ieta()) == itIeta)  input.push_back(1.f);
-		else					input.push_back(0.f);
-	    }
-
-	    data1->push_back(input);
-	}
-
-	input1.toServer(data1);
+      //FACILE does no time reco
+      HBHERecHit rh = HBHERecHit(hcalIds_[iB], rhE, 0.f, 0.f);
+      out->push_back(rh);
     }
+    iEvent.put(std::move(out));
+  }
 
-    void produce(edm::Event& iEvent, edm::EventSetup const& iSetup, Output const& iOutput) override {
-	std::unique_ptr<HBHERecHitCollection> out;
-	out = std::make_unique<HBHERecHitCollection>();
-	out->reserve(hcalIds_.size());
-
-	const auto& output1 = iOutput.begin()->second;
-	const auto& outputs = output1.fromServer<float>();
-
-	for(std::size_t iB = 0; iB < hcalIds_.size(); iB++){
-
-	    float rhE = outputs[iB][0];
-	    if(rhE < 0. or std::isnan(rhE) or std::isinf(rhE)) rhE = 0; 
-
-	    //FACILE does no time reco 
-	    HBHERecHit rh = HBHERecHit(hcalIds_[iB],rhE,0.f,0.f);
-	    out->push_back(rh); 
-	}
-	iEvent.put(std::move(out));	
-    }
-
-    static void fillDescriptions(edm::ConfigurationDescriptions & descriptions) {
-        edm::ParameterSetDescription desc;
-        TritonClient::fillPSetDescription(desc);
- 	desc.add<edm::InputTag>("ChannelInfoName");
-        descriptions.add("FacileHcalReconstructor",desc);
-    }
-
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+    edm::ParameterSetDescription desc;
+    TritonClient::fillPSetDescription(desc);
+    desc.add<edm::InputTag>("ChannelInfoName");
+    descriptions.add("FacileHcalReconstructor", desc);
+  }
 
 private:
-    edm::InputTag fChannelInfoName_;   
-    edm::EDGetTokenT<HBHEChannelInfoCollection> fTokChannelInfo_;
-    std::vector<HcalDetId> hcalIds_;
+  edm::InputTag fChannelInfoName_;
+  edm::EDGetTokenT<HBHEChannelInfoCollection> fTokChannelInfo_;
+  std::vector<HcalDetId> hcalIds_;
+  edm::ESGetToken<HcalTopology, HcalRecNumberingRecord> htopoToken_;
 };
 
 DEFINE_FWK_MODULE(FacileHcalReconstructor);
